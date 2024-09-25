1dd3f53a971a97e9630892b7b697abfd11561bcd
ahmed2ata <116737655+ahmed2ata@users.noreply.github.com>
2024-09-26 01:35:25 +0300

    Add files via upload

---
+++ HTML scraping.js
@@ -0,0 +1,244 @@
+let html = `<div class="ld-item-list-items  ld-item-list-4467" id="ld-item-list-4467" data-ld-expand-id="ld-item-list-4467" data-ld-expand-list="true">
+		
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4474 is_not_sample learndash-complete" id="ld-expand-4474" data-ld-expand-id="ld-expand-4474" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/01-prepare-development-environment/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				01 &#8211; Prepare Development Environment
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4476 is_not_sample learndash-complete" id="ld-expand-4476" data-ld-expand-id="ld-expand-4476" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/02-node-js-application-files-organization/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				02 &#8211; Node.js application files organization
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4478 is_not_sample learndash-complete" id="ld-expand-4478" data-ld-expand-id="ld-expand-4478" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/03-variables-introduction/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				03 &#8211; Variables
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4480 is_not_sample learndash-complete" id="ld-expand-4480" data-ld-expand-id="ld-expand-4480" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/04-variables-hands-on/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				04 &#8211; Variables &#8211; hands-on
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4505 is_not_sample learndash-complete" id="ld-expand-4505" data-ld-expand-id="ld-expand-4505" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/05-operators-arithmetic/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				05 &#8211; Operators &#8211; Arithmetic
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4524 is_not_sample learndash-complete" id="ld-expand-4524" data-ld-expand-id="ld-expand-4524" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/06-operators-arithmetic-priorities-order-of-operations/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				06 &#8211; Operators &#8211; Arithmetic &#8211; Priorities (order of operations)
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4528 is_not_sample learndash-complete" id="ld-expand-4528" data-ld-expand-id="ld-expand-4528" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/07-operators-arithmetic-hands-on/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				07 &#8211; Operators &#8211; Arithmetic &#8211; hands-on
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4534 is_not_sample learndash-complete" id="ld-expand-4534" data-ld-expand-id="ld-expand-4534" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/08-operators-assignments/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				08 &#8211; Operators &#8211; Assignments
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4547 is_not_sample learndash-complete" id="ld-expand-4547" data-ld-expand-id="ld-expand-4547" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/exercises-01/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				Exercises &#8211; 01
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->
+	
+<div class="ld-item-list-item ld-item-lesson-item ld-lesson-item-4568 is_not_sample learndash-complete" id="ld-expand-4568" data-ld-expand-id="ld-expand-4568" >
+	<div class="ld-item-list-item-preview">
+		
+		<a class="ld-item-name ld-primary-color-hover" href="https://cloudnativebasecamp.com/lessons/09-operators-comparison/">
+			<div class="ld-status-icon ld-status-complete ld-secondary-background"><span class="ld-icon-checkmark ld-icon"></span></div>			<div class="ld-item-title">
+				09 &#8211; Operators &#8211; Comparison
+			</div> <!--/.ld-item-title-->
+		</a>
+
+		
+		<div class="ld-item-details">
+					</div> <!--/.ld-item-details-->
+
+		
+	</div> <!--/.ld-item-list-item-preview-->
+	</div> <!--/.ld-item-list-item-->`;
+
+/*"ld-item-title">
+				01 &#8211; Prepare Development Environment
+			</div>*/    
+
+let items = [];
+let start_index = -1;
+let end_index = -1;
+
+for (let i = 0; i < html.length; i++) {
+    start_index= html.indexOf('"ld-item-title"', i);
+	if (start_index == -1)  break;
+	i = start_index ;
+
+	end_index= html.indexOf('</div>', i);
+	if (end_index == -1)  break;
+	i = end_index ;
+    
+	let items = html.substring(start_index, end_index);
+/* we are here 
+"ld-item-title">
+				01 &#8211; Prepare Development Environment */
+
+    items = items.replace('"ld-item-title">','');
+/* we are here 
+				01 &#8211; Prepare Development Environment
+*/	
+    items = items.trim();
+	items= items.replace(/&#8211;+/g, '-');	
+/*finally we have that 01 - Prepare Development Environment
+*/	
+	items= items.replace(/^0+(\d)/, '$1');
+	console.log(items)
+
+}
+//				04 &#8211; Variables &#8211; hands-on
+//				03 &#8211; Variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// function to get all indexes of a reapeted word
+let text = "call me ahmed and don/'t call me ataa call her israaa and don/'t call her mai"
+let index= -1
+function AllindexOf (str,pattern) {
+    for (let i =0; i < str.length; i ++) {
+        index = text.indexOf(pattern,i);
+        i = index;
+        if (index == -1) break;
+        console.log(index);
+    }
+}
+AllindexOf(text,"ahmed");
+console.log("--------------------------------------------------");*/
+
+
+
